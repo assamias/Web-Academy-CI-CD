019f2a1ad007b9522eb793c281605131a2f5f5ec
Alexandre Samias <alexandresamias66@gmail.com>
2025-12-23 02:17:50 -0300

    fix: eslint config for node environment

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,27 @@
 module.exports = {
-  env: {
-    browser: true,
-    node: true,
-    es2021: true,
-  },
-  extends: "eslint:recommended",
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "script",
-  },
-  rules: {},
-};
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+}
